99390747b8452329ea39c1ca59733ea8aad01538
Akbar Binaji <akbar.binaji@gmail.com>
2020-09-10 22:20:29 +0700

    test with rear camera

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,9 +1,15 @@
 var video = document.getElementById('video');
+var constraints = { 
+    audio: false, 
+    video: { 
+        facingMode: 'environment' 
+    } 
+};
 
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
         video.play();
